48c2a74dfc0ea5e5ac8f1c002013e673d29ba133
Milan Malfait <m.malfait@ucl.ac.uk>
2023-11-06 15:08:25 +0000

    Some more nvim settings

--- .config/nvim-custom/init.lua
+++ .config/nvim-custom/init.lua
@@ -1,10 +1,15 @@
 -- For configuring nvim options
 
+local set = vim.opt -- set options
+-- set.tabstop = 4
+-- set.softtabstop = 4
+-- set.shiftwidth = 4
+
 -- Add width marker
-vim.opt.colorcolumn = "100"
+set.colorcolumn = "100"
 
 -- Set relative line numbers by default
-vim.opt.relativenumber = true
+set.relativenumber = true
 
 -- Open nivmtree when in directory
 local function open_nvim_tree(data)
