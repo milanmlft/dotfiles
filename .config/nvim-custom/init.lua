a789231370752183b16df398a68d2acbef7d325b
Milan Malfait <m.malfait@ucl.ac.uk>
2023-11-10 16:08:26 +0000

    Set tabwidth in neovim

--- .config/nvim-custom/init.lua
+++ .config/nvim-custom/init.lua
@@ -1,9 +1,9 @@
 -- For configuring nvim options
 
 local set = vim.opt -- set options
--- set.tabstop = 4
--- set.softtabstop = 4
--- set.shiftwidth = 4
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
 
 -- Add width marker
 set.colorcolumn = "100"
