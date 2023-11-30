24fca5fd6202cdf1756e37a5e26152c11771411a
Milan Malfait <m.malfait@ucl.ac.uk>
2023-11-30 10:09:19 +0000

    Mostly formatting

--- .config/nvim-custom/init.lua
+++ .config/nvim-custom/init.lua
@@ -1,34 +1,31 @@
 -- For configuring nvim options
-
 local set = vim.opt -- set options
 set.tabstop = 4
 set.softtabstop = 4
 set.shiftwidth = 4
 
 -- Add width marker
-set.colorcolumn = "100"
+set.colorcolumn = "88"
 
 -- Set relative line numbers by default
 set.relativenumber = true
 
 -- Open nivmtree when in directory
 local function open_nvim_tree(data)
-  -- buffer is a directory
-  local directory = vim.fn.isdirectory(data.file) == 1
+    -- buffer is a directory
+    local directory = vim.fn.isdirectory(data.file) == 1
 
-  if not directory then
-    return
-  end
+    if not directory then return end
 
-  -- change to the directory
-  vim.cmd.cd(data.file)
+    -- change to the directory
+    vim.cmd.cd(data.file)
 
-  -- open the tree
-  require("nvim-tree.api").tree.open()
+    -- open the tree
+    require("nvim-tree.api").tree.open()
 end
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+vim.api.nvim_create_autocmd({"VimEnter"}, {callback = open_nvim_tree})
 
 local function reverse_highlight_colors()
-  vim.cmd('hi Visual cterm=reverse gui=reverse')
+    vim.cmd('hi Visual cterm=reverse gui=reverse')
 end
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = reverse_highlight_colors })
+vim.api.nvim_create_autocmd({"VimEnter"}, {callback = reverse_highlight_colors})
