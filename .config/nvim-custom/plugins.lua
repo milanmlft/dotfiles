db85f80f1cedbd2d5f8fdde7fad2155a9cd7119e
Milan Malfait <m.malfait@ucl.ac.uk>
2023-12-07 10:30:37 +0000

    Install zen-mode nvim plugin

--- .config/nvim-custom/plugins.lua
+++ .config/nvim-custom/plugins.lua
@@ -145,6 +145,13 @@ local plugins = {
         build = "cd app && yarn install",
         init = function() vim.g.mkdp_filetypes = {"markdown"} end,
         ft = {"markdown"}
+    }, {
+        "folke/zen-mode.nvim",
+        cmd = "ZenMode",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+        }
     }
 }
 return plugins
