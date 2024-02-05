4b3b8c4f2d3a43318f79ad8763b37e170dfa9876
Milan Malfait <m.malfait@ucl.ac.uk>
2024-02-05 22:14:24 +0100

    Neovim configs, formatting

--- .config/nvim-custom/configs/null-ls.lua
+++ .config/nvim-custom/configs/null-ls.lua
@@ -11,28 +11,28 @@ local opts = {
         null_ls.builtins.diagnostics.mypy.with({
             extra_args = function()
                 local virtual = os.getenv("VIRTUAL_ENV") .. "/bin" or
-                    os.getenv("PYENV_ROOT") .. "/shims" or
-                    "/usr/bin"
-                return { "--python-executable", virtual .. "/python3" }
+                                    os.getenv("PYENV_ROOT") .. "/shims" or
+                                    "/usr/bin"
+                return {"--python-executable", virtual .. "/python3"}
             end
         }), null_ls.builtins.formatting.gofmt, -- go
         null_ls.builtins.formatting.goimports_reviser,
         null_ls.builtins.formatting.golines,
         null_ls.builtins.formatting.clang_format, -- c++
-        null_ls.builtins.formatting.lua_format,   -- lua
-        -- null_ls.builtins.formatting.markdownlint, -- markdown
-        null_ls.builtins.diagnostics.cmake_lint,  -- cmake
+        null_ls.builtins.formatting.lua_format, -- lua
+        null_ls.builtins.diagnostics.cmake_lint, -- cmake
         null_ls.builtins.formatting.cmake_format,
+        null_ls.builtins.formatting.prettierd,
         null_ls.builtins.formatting.prettierd
     },
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_clear_autocmds({group = augroup, buffer = bufnr})
             vim.api.nvim_create_autocmd("BufWritePre", {
                 group = augroup,
                 buffer = bufnr,
                 callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr })
+                    vim.lsp.buf.format({bufnr = bufnr})
                 end
             })
         end
