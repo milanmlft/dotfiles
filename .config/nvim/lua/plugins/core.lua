2ffb1d17e04b6f711e4756ae10e606e68101e52e
Milan Malfait <m.malfait@ucl.ac.uk>
2024-02-18 20:46:29 +0000

    Set catppuccin theme settings in neovim

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -5,4 +5,17 @@ return {
       colorscheme = "catppuccin",
     },
   },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "macchiato",
+      transparent_background = true,
+      styles = {
+        comments = { "italic" },
+      },
+    },
+  },
 }
