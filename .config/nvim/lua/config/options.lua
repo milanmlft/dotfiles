5e363df24bb17d0c59503415452f9954a3e5feec
Milan Malfait <m.malfait@ucl.ac.uk>
2024-02-18 20:45:58 +0000

    Don't know why I had this

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -11,9 +11,3 @@ set.linebreak = true
 set.scrolloff = 8
 set.textwidth = 100
 set.colorcolumn = "100"
-
--- Reverse highlight colors
-local function reverse_highlight_colors()
-  vim.cmd("hi Visual cterm=reverse gui=reverse")
-end
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = reverse_highlight_colors })
