d91fb1ab9ceba3e11466f5f9163d58b7515b78d9
Milan Malfait <m.malfait@ucl.ac.uk>
2024-02-19 11:57:55 +0000

    Add primeagen's vim settings

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,10 +4,35 @@
 
 local set = vim.opt
 
-set.ignorecase = true
+set.number = true
+set.relativenumber = true
 
-set.relativenumber = false
-set.linebreak = true
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
+
+set.smartindent = true
+
+set.wrap = true
+
+-- Disable swap files and use undotree instead
+set.swapfile = false
+set.backup = false
+set.undodir = os.getenv("HOME") .. "/.vim/undodir"
+set.undofile = true
+
+set.hlsearch = false
+set.incsearch = true
+
+set.termguicolors = true
+
+-- Scrolling
 set.scrolloff = 8
+set.signcolumn = "yes"
+
+-- Faster updatetime
+set.updatetime = 50
+
 set.textwidth = 100
 set.colorcolumn = "100"
