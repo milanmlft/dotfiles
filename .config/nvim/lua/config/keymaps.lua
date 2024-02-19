5e950d4a807c092cb7fca2ef2d1b3b03569f07a4
Milan Malfait <m.malfait@ucl.ac.uk>
2024-02-19 11:57:42 +0000

    Add primeagen's keymaps (some of them)

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,29 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("n", "<leader>pv", vim.cmd.Ex)
+
+-- Move lines up and down in Visual mode
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor in same position for various operations
+map("n", "J", "mzJ`z")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- Pasting without overwriting the paste buffer!
+map("x", "<leader>p", '"_dP')
+
+-- Diable Q!
+map("n", "Q", "<nop>")
+
+-- Replace word under cursor
+map("n", "<leader>r", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+
+map("n", "<leader>u", vim.cmd.UndotreeToggle)
