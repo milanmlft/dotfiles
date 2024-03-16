e8eb17b21a957616fb01a4459e3b96dbf622484e
Milan Malfait <m.malfait@ucl.ac.uk>
2024-03-16 18:46:55 +0000

    nvim: add cmake keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,6 +3,7 @@
 -- Add any additional keymaps here
 
 local map = vim.keymap.set
+local wk = require("which-key")
 
 map("n", "<leader>pv", vim.cmd.Ex)
 
@@ -20,10 +21,17 @@ map("n", "N", "Nzzzv")
 -- Pasting without overwriting the paste buffer!
 map("x", "<leader>p", '"_dP')
 
--- Diable Q!
+-- Disable Q!
 map("n", "Q", "<nop>")
 
 -- Replace word under cursor
-map("n", "<leader>r", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+map("n", "<leader>r", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", { desc = "Replace word under cursor" })
 
 map("n", "<leader>U", vim.cmd.UndotreeToggle, { desc = "Toggle Undotree" })
+
+-- cmake
+wk.register({
+  C = { name = "+CMake" },
+})
+map("n", "<leader>Cg", ":CMakeGenerate<CR>", { desc = "CMake Generate" })
+map("n", "<leader>Cb", ":CMakeBuild<CR>", { desc = "CMake Build" })
