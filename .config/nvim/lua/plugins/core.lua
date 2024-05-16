c548a6f37a927d272d3cd30bd05fb882c4abb915
Milan Malfait <m.malfait@ucl.ac.uk>
2024-05-16 15:37:28 +0100

    Revert "Switch to duskfox theme"
    This reverts commit 59363f7138ccf4c5d6628a985e8dac93c3b995a7.

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -2,17 +2,19 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "duskfox",
+      colorscheme = "catppuccin",
     },
   },
 
   {
-    "EdenEast/nightfox.nvim",
-    name = "nightfox",
-    priority = 100,
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
     opts = {
-      options = {
-        transparent = true,
+      flavour = "macchiato",
+      transparent_background = true,
+      styles = {
+        comments = { "italic" },
       },
     },
   },
