3702fe0ebd1626f0083973bdd4034ab65b30aba7
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-09-09 17:33:36 +0100

    Update nvim tabstop

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -12,8 +12,8 @@ set.smarttab = true
 set.expandtab = true
 
 -- Set tab stops to different length than indentation width, to reduce the chance of mixing tabs and spaces
-set.tabstop = 8
-set.softtabstop = 8
+set.tabstop = 4
+set.softtabstop = 4
 
 set.smartindent = true
 
