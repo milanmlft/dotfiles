1c67ded54d05923c00373565a3470632a1e4b152
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-03-04 14:07:23 +0000

    Add formatting for quarto

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,15 +1,19 @@
-local sql_ft = { "sql", "mysql", "plsql" }
-
 return {
   "stevearc/conform.nvim",
   optional = true,
-  opts = function(_, opts)
-    opts.formatters.sqlfluff = {
-      args = { "format", "--dialect=ansi", "-" },
-    }
-    for _, ft in ipairs(sql_ft) do
-      opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
-      table.insert(opts.formatters_by_ft[ft], "sqlfluff")
-    end
-  end,
+  opts = {
+    formatters = {
+      prettier = {
+        options = {
+          ext_parsers = {
+            qmd = "markdown",
+          },
+        },
+      },
+    },
+    formatters_by_ft = {
+      quarto = { "prettierd", "prettier" },
+      markdown = { "prettierd", "prettier" },
+    },
+  },
 }
