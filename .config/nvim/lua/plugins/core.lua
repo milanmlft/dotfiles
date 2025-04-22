aa4fd148fb22a5fa254495bb2b733cf7af341c5f
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-04-22 17:33:54 +0200

    Lazyvim updates

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -2,20 +2,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nightfox",
-    },
-  },
-
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      flavour = "macchiato",
-      transparent_background = true,
-      styles = {
-        comments = { "italic" },
-      },
+      colorscheme = "carbonfox",
     },
   },
   {
