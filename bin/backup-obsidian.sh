647984c6cf722bd53e07ab5d443bc035a6dfd76c
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-08-23 13:17:10 +0200

    :sparkles: Rework obsidian backup script
    - Store obsidian backups in icloud
    - Exclude `.git` dir from archive

--- bin/backup-obsidian.sh
+++ bin/backup-obsidian.sh
@@ -5,8 +5,10 @@ set -euxo pipefail
 OBSIDIAN_DIR=~/obsidian
 VAULT_DIR=home
 DATE=$(date +%Y-%m-%d)
-ZIP_FILE=${DATE}_${VAULT_DIR}.zip
+
+ICLOUD_DIR=${OBSIDIAN_DIR}/obsidian-icloud
+ZIP_FILE=${ICLOUD_DIR}/${DATE}_${VAULT_DIR}.zip
 
 cd "$OBSIDIAN_DIR"
 echo "Zipping $VAULT_DIR to $ZIP_FILE"
-zip -r "$ZIP_FILE" "$VAULT_DIR"
+zip -r ${ZIP_FILE} ${VAULT_DIR}/* -x ${VAULT_DIR}/.git/
