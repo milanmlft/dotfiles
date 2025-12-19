ba6a5f9aa32d0a68cb1089607382bb885381790d
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-12-19 17:05:41 +0100

    Set up formatters

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -154,9 +154,6 @@ return {
 
       -- Ensure the servers and tools above are installed
       local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        "stylua", -- Used to format Lua code
-      })
       require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
       require("mason-lspconfig").setup({
