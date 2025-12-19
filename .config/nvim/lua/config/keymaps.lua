56621a977a6006aa19591b6ab3a5106d5277e3f1
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-12-19 11:14:57 +0100

    Kickstart.nvim keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,13 +5,21 @@ vim.g.maplocalleader = ' '
 -- [[ Basic Keymaps ]]
 -- See `:h vim.keymap.set()`, `:h mapping`, `:h keycodes`
 
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
 -- Use <Esc> to exit terminal mode
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- Map <C-j>, <C-k>, <C-h>, <C-l> to navigate between windows
-vim.keymap.set({ 'n' }, '<C-h>', '<C-w>h')
-vim.keymap.set({ 'n' }, '<C-j>', '<C-w>j')
-vim.keymap.set({ 'n' }, '<C-k>', '<C-w>k')
-vim.keymap.set({ 'n' }, '<C-l>', '<C-w>l')
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
-
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -51,7 +51,3 @@ vim.o.scrolloff = 10
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s) See `:help 'confirm'`
 vim.o.confirm = true
-
--- Add the "nohlsearch" package to automatically turn off search highlighting after
--- 'updatetime' and when going to insert mode
-vim.cmd('packadd! nohlsearch')
