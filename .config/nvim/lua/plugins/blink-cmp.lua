a3d68d2d5002fc099a14bfd19eb487d4ca91239a
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-12-19 17:19:59 +0100

    Add todo-comments

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -38,7 +38,7 @@ return {
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = "default",
+        preset = "enter",
       },
 
       appearance = {
