b55a06bc6ec8834c2cc2660a1d189a422aec88a9
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-12-19 09:51:30 +0100

    Start with `$VIMRUNTIME/example_init.lua`

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,72 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+-- Set <space> as the leader key
+vim.g.mapleader = ' '
+
+-- [[ Setting options ]] See `:h vim.o`
+
+-- Print the line number in front of each line
+vim.o.number = true
+-- Use relative line numbers
+vim.o.relativenumber = true
+
+-- Sync clipboard between OS and Neovim.
+-- vim.api.nvim_create_autocmd('UIEnter', {
+--   callback = function()
+--     vim.o.clipboard = 'unnamedplus'
+--   end,
+-- })
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Highlight the line where the cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s) See `:help 'confirm'`
+vim.o.confirm = true
+
+-- [[ Set up keymaps ]] See `:h vim.keymap.set()`, `:h mapping`, `:h keycodes`
+
+-- Use <Esc> to exit terminal mode
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+
+-- Map <C-j>, <C-k>, <C-h>, <C-l> to navigate between windows
+vim.keymap.set({ 'n' }, '<C-h>', '<C-w>h')
+vim.keymap.set({ 'n' }, '<C-j>', '<C-w>j')
+vim.keymap.set({ 'n' }, '<C-k>', '<C-w>k')
+vim.keymap.set({ 'n' }, '<C-l>', '<C-w>l')
+
+
+-- [[ Basic Autocommands ]].
+
+-- Highlight when yanking (copying) text.
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+
+-- [[ Create user commands ]]
+-- See `:h nvim_create_user_command()` and `:h user-commands`
+
+-- Create a command `:GitBlameLine` that print the git blame for the current line
+vim.api.nvim_create_user_command('GitBlameLine', function()
+  local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
+  local filename = vim.api.nvim_buf_get_name(0)
+  print(vim.fn.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }))
+end, { desc = 'Print the git blame for the current line' })
+
+
+-- [[ Add optional packages ]]
+-- Nvim comes bundled with a set of packages that are not enabled by
+-- default. You can enable any of them by using the `:packadd` command.
+
+-- For example, to add the "nohlsearch" package to automatically turn off search highlighting after
+-- 'updatetime' and when going to insert mode
+vim.cmd('packadd! nohlsearch')
