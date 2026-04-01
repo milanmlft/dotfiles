7ea1832c78f51df999ecffe4c43695a2a1b7de0d
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2026-04-01 16:24:08 +0100

    Speed up zsh startup

--- .config/zsh/paths.zsh
+++ .config/zsh/paths.zsh
@@ -1,25 +1,22 @@
-## Set config location for tms
-export TMS_CONFIG_FILE="$XDG_CONFIG_HOME/tms/config.toml"
-
 ## Personal scripts
 export PATH="$PATH:$HOME/bin"
 
-export PATH="$(brew --prefix)/opt/libxml2/bin:$PATH"
+export PATH="/opt/homebrew/opt/libxml2/bin:$PATH"
 
 # allows Coreutils package to be used without 'g' prefix before each command.
-export PATH="$(brew --prefix)/opt/coreutils/libexec/gnubin:$PATH"
-export MANPATH="$(brew --prefix)/opt/coreutils/libexec/gnuman:$MANPATH"
+export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+export MANPATH="/opt/homebrew/opt/coreutils/libexec/gnuman:$MANPATH"
 
 ## Set C/C++ compilers
-export PATH="$(brew --prefix)/opt/llvm/bin:$PATH"
-export PATH="$(brew --prefix)/opt/gcc/bin:$PATH"
+export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+export PATH="/opt/homebrew/opt/gcc/bin:$PATH"
 
 ## For CMake
-export CC="$(brew --prefix)/opt/llvm/bin/clang"
-export CXX="$(brew --prefix)/opt/llvm/bin/clang++"
-export CMAKE_PREFIX_PATH="$(brew --prefix)"
-export CMAKE_C_COMPILER="$(brew --prefix)/opt/llvm/bin/clang"
-export CMAKE_CXX_COMPILER="$(brew --prefix)/opt/llvm/bin/clang++"
+export CC="/opt/homebrew/opt/llvm/bin/clang"
+export CXX="/opt/homebrew/opt/llvm/bin/clang++"
+export CMAKE_PREFIX_PATH="/opt/homebrew"
+export CMAKE_C_COMPILER="/opt/homebrew/opt/llvm/bin/clang"
+export CMAKE_CXX_COMPILER="/opt/homebrew/opt/llvm/bin/clang++"
 export CMAKE_FIND_FRAMEWORK="LAST"
 export CMAKE_FIND_APPBUNDLE="NEVER"
 export CMAKE_EXPORT_COMPILE_COMMANDS="ON"
@@ -27,10 +24,10 @@ export CMAKE_EXPORT_COMPILE_COMMANDS="ON"
 export GPG_TTY=$TTY
 
 ## brew autocompletion
-FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+FPATH="/opt/homebrew/share/zsh/site-functions:${FPATH}"
 
 ## Use GNU sed
-export PATH="$(brew --prefix)/opt/gnu-sed/libexec/gnubin:$PATH"
+export PATH="/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH"
 
 ## Go
 export GOPATH=$HOME/go
