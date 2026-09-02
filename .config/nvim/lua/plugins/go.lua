5e036e92c4a729b3239785613bd4a8d988e442c7
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2026-09-02 11:39:34 +0100

    LazyVim: add golang debugger and tests

--- .config/nvim/lua/plugins/go.lua
+++ .config/nvim/lua/plugins/go.lua
@@ -92,4 +92,38 @@ return {
       },
     },
   },
+
+  -- ── Debugger ──────────────────────────────────────────────────────────────
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      {
+        "mason-org/mason.nvim",
+        opts = { ensure_installed = { "delve" } },
+      },
+      {
+        "leoluz/nvim-dap-go",
+        opts = {},
+      },
+    },
+  },
+
+  -- ── Testing  ────────────────────────────────────────────────────────────────
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "fredrikaverpil/neotest-golang",
+    },
+    opts = {
+      adapters = {
+        ["neotest-golang"] = {
+          -- Here we can set options for neotest-golang, e.g.
+          -- go_test_args = { "-v", "-race", "-count=1", "-timeout=60s" },
+          dap_go_enabled = true, -- requires leoluz/nvim-dap-go
+        },
+      },
+    },
+  },
 }
